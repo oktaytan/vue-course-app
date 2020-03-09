546ceb7b9dc7c50faab55a1f99a1661b2f19b492
Oktay Tanrıkulu <oktaytanrikulu@gmail.com>
2020-03-09 19:55:16 +0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,45 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+	<div id="app">
+		<b-navbar
+			toggleable="sm"
+			type="light"
+			variant="light"
+			style="min-height: 100px"
+			class="border bg-white"
+		>
+			<div class="bar fixed-top w-100 bg-primary"></div>
+			<b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+
+			<b-container>
+				<b-navbar-brand to="/">CourseApp</b-navbar-brand>
+
+				<b-collapse id="nav-text-collapse" is-nav>
+					<b-navbar-nav class="ml-auto">
+						<b-nav-item to="/">Courses</b-nav-item>
+						<b-nav-item to="/about">About</b-nav-item>
+					</b-navbar-nav>
+				</b-collapse>
+			</b-container>
+		</b-navbar>
+
+		<b-container>
+			<b-row>
+				<b-card class="w-100 my-5 rounded-lg" style="min-height: 70vh">
+					<router-view />
+				</b-card>
+			</b-row>
+		</b-container>
+
+		<b-container fluid class="fixd-bottom text-center text-secondary">
+			<div>{{
+				`&copy; Codeigniter Vue Course App ${new Date().getFullYear()}`
+			}}</div>
+		</b-container>
+	</div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.bar {
+	height: 8px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import './scss/custom.scss';
+import axios from 'axios';
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+axios.defaults.baseURL = 'http://localhost/codeigniter-api/api';
+Vue.prototype.axios = axios;
 
 Vue.config.productionTip = false
 
-new Vue({
+window.vm = new Vue({
   router,
   store,
   render: h => h(App)
